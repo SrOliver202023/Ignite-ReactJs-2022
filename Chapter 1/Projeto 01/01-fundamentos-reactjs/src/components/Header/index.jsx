811460d18e209f9368122768d3e79c53907fa7fa
SrOliver202023 <sroliver202023@gmail.com>
2022-06-06 01:08:43 -0300

    Chapter 1 | Estrutura da aplicação | 03 - Componente: Header

--- Chapter 1/Projeto 01/01-fundamentos-reactjs/src/components/Header/index.jsx
+++ Chapter 1/Projeto 01/01-fundamentos-reactjs/src/components/Header/index.jsx
@@ -1,8 +1,11 @@
 import style from './style.module.css';
 
+import IgniteLogo from '../../assets/IgniteLogo.svg';
+
 export function Header() {
   return (
     <header className={style.Header}>
+      <img src={IgniteLogo} alt='Logotipo do Ignite' />
       <strong>Ignite Feed</strong>
     </header>
 
