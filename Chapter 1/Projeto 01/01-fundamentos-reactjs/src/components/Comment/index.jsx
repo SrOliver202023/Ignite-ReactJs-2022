bba58a93f37973084258b2b42d48f9ce8087ec7a
SrOliver202023 <sroliver202023@gmail.com>
2022-06-08 19:06:27 -0300

    Chapter 1 | Estrutura da aplicação | 10 - Estilizações do Comentário

--- Chapter 1/Projeto 01/01-fundamentos-reactjs/src/components/Comment/index.jsx
+++ Chapter 1/Projeto 01/01-fundamentos-reactjs/src/components/Comment/index.jsx
@@ -4,33 +4,38 @@ import styles from './Comment.module.css';
 
 export function Comment() {
   return (
-    <div className={styles.commentBox}>
+    <div className={styles.comment}>
       <img
         src='https://github.com/sroliver202023.png'
       />
 
-      <div className={styles.commentContent}>
-        <header>
-          <div className={styles.authorAndTime}>
-            <strong>Emmerson Oliveira</strong>
-            <time
-              title='11 de Maio às 08:13h'
-              dateTime='2022-05-11 08:13:30'>
-              Cerca de 1h atrás
-            </time>
-          </div>
-          <button title="Deletar comentário">
-            <Trash size={20} />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Emmerson Oliveira</strong>
+              <time
+                title='11 de Maio às 08:13h'
+                dateTime='2022-05-11 08:13:30'>
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+
+          </header>
+          <p>Muito bom Devon, parabéns!!  👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
           </button>
-        </header>
-        <p>Muito bom Devon, parabéns!!  👏👏</p>
+        </footer>
       </div>
-      <footer>
-        <button>
-          <ThumbsUp />
-          Aplaudir <span>20</span>
-        </button>
-      </footer>
+
     </div>
   );
 }
