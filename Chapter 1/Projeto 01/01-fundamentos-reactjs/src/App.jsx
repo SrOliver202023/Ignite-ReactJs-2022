7577da7d356a9cb6dd26511c1cc340b41dc2d8c9
SrOliver202023 <sroliver202023@gmail.com>
2022-06-09 11:16:25 -0300

    Chapter 1 | Os motores do React | 01 - Iterando no JSX

--- Chapter 1/Projeto 01/01-fundamentos-reactjs/src/App.jsx
+++ Chapter 1/Projeto 01/01-fundamentos-reactjs/src/App.jsx
@@ -5,8 +5,41 @@ import styles from './App.module.css';
 import { Header } from "./components/Header";
 import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post';
-import { Avatar } from './components/Avatar';
 
+// author: { avatar_url:"", name:"", role:""}
+// publishAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "github.com/sroliver202023.png",
+      name: "Emmerson Oliveira",
+      role: "Student @Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishAt: new Date('2022-06-05 15:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "Educator @Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishAt: new Date('2022-06-08 20:00:00')
+  }
+];
 
 function App() {
 
@@ -16,14 +49,21 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {
+            posts.map((post) => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishAt={post.publishAt}
+                />
+              );
+            })
+          }
         </main>
       </div>
     </div>
   );
-
 }
 
 export default App;
